751771c8e07f175493e7e39416223705e2d82d82
Alex Antonio Victoria Vázquez <23620281@tlaxiaco.tecnm.mx>
2025-04-16 17:56:21 -0600

    Primer commit proyecto de relojes con hilos - plantilla

---
+++ HilosRelojes/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace HilosRelojes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHora1 = new System.Windows.Forms.Label();
+            this.lblHora2 = new System.Windows.Forms.Label();
+            this.lblHora3 = new System.Windows.Forms.Label();
+            this.lblHora4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.btnPausa = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblInicar = new System.Windows.Forms.Label();
+            this.lblPausa = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblSalir = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHora1
+            // 
+            this.lblHora1.AutoSize = true;
+            this.lblHora1.Font = new System.Drawing.Font("MS Reference Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblHora1.Location = new System.Drawing.Point(151, 159);
+            this.lblHora1.Name = "lblHora1";
+            this.lblHora1.Size = new System.Drawing.Size(166, 59);
+            this.lblHora1.TabIndex = 0;
+            this.lblHora1.Text = "label1";
+            // 
+            // lblHora2
+            // 
+            this.lblHora2.AutoSize = true;
+            this.lblHora2.Font = new System.Drawing.Font("MS Reference Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblHora2.Location = new System.Drawing.Point(645, 159);
+            this.lblHora2.Name = "lblHora2";
+            this.lblHora2.Size = new System.Drawing.Size(166, 59);
+            this.lblHora2.TabIndex = 1;
+            this.lblHora2.Text = "label2";
+            // 
+            // lblHora3
+            // 
+            this.lblHora3.AutoSize = true;
+            this.lblHora3.Font = new System.Drawing.Font("MS Reference Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblHora3.Location = new System.Drawing.Point(151, 357);
+            this.lblHora3.Name = "lblHora3";
+            this.lblHora3.Size = new System.Drawing.Size(166, 59);
+            this.lblHora3.TabIndex = 2;
+            this.lblHora3.Text = "label3";
+            // 
+            // lblHora4
+            // 
+            this.lblHora4.AutoSize = true;
+            this.lblHora4.Font = new System.Drawing.Font("MS Reference Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora4.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblHora4.Location = new System.Drawing.Point(645, 357);
+            this.lblHora4.Name = "lblHora4";
+            this.lblHora4.Size = new System.Drawing.Size(166, 59);
+            this.lblHora4.TabIndex = 3;
+            this.lblHora4.Text = "label4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tlaxiaco, Oaxaca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(659, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "New York";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(178, 421);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Madrid";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(686, 421);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tokio";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(287, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(392, 64);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Zonas horarias";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.lblSalir);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lblPausa);
+            this.panel1.Controls.Add(this.lblInicar);
+            this.panel1.Controls.Add(this.BtnSalir);
+            this.panel1.Controls.Add(this.btnPausa);
+            this.panel1.Controls.Add(this.btnIniciar);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblHora4);
+            this.panel1.Controls.Add(this.lblHora3);
+            this.panel1.Controls.Add(this.lblHora2);
+            this.panel1.Controls.Add(this.lblHora1);
+            this.panel1.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(945, 629);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BtnSalir.Image = global::HilosRelojes.Properties.Resources.stop_button;
+            this.BtnSalir.Location = new System.Drawing.Point(694, 502);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(75, 52);
+            this.BtnSalir.TabIndex = 11;
+            this.BtnSalir.UseVisualStyleBackColor = false;
+            // 
+            // btnPausa
+            // 
+            this.btnPausa.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnPausa.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnPausa.Image = global::HilosRelojes.Properties.Resources.pause;
+            this.btnPausa.Location = new System.Drawing.Point(450, 502);
+            this.btnPausa.Name = "btnPausa";
+            this.btnPausa.Size = new System.Drawing.Size(75, 52);
+            this.btnPausa.TabIndex = 10;
+            this.btnPausa.UseVisualStyleBackColor = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnIniciar.Image = global::HilosRelojes.Properties.Resources.play_button__3_;
+            this.btnIniciar.Location = new System.Drawing.Point(184, 502);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(85, 52);
+            this.btnIniciar.TabIndex = 9;
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            // 
+            // lblInicar
+            // 
+            this.lblInicar.AutoSize = true;
+            this.lblInicar.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicar.Location = new System.Drawing.Point(192, 557);
+            this.lblInicar.Name = "lblInicar";
+            this.lblInicar.Size = new System.Drawing.Size(71, 26);
+            this.lblInicar.TabIndex = 12;
+            this.lblInicar.Text = "Iniciar";
+            // 
+            // lblPausa
+            // 
+            this.lblPausa.AutoSize = true;
+            this.lblPausa.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPausa.Location = new System.Drawing.Point(454, 556);
+            this.lblPausa.Name = "lblPausa";
+            this.lblPausa.Size = new System.Drawing.Size(67, 26);
+            this.lblPausa.TabIndex = 13;
+            this.lblPausa.Text = "Pausa";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(875, 603);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 26);
+            this.label8.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(919, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 26);
+            this.label9.TabIndex = 15;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // lblSalir
+            // 
+            this.lblSalir.AutoSize = true;
+            this.lblSalir.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalir.Location = new System.Drawing.Point(706, 555);
+            this.lblSalir.Name = "lblSalir";
+            this.lblSalir.Size = new System.Drawing.Size(54, 26);
+            this.lblSalir.TabIndex = 16;
+            this.lblSalir.Text = "Salir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 629);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora1;
+        private System.Windows.Forms.Label lblHora2;
+        private System.Windows.Forms.Label lblHora3;
+        private System.Windows.Forms.Label lblHora4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Button btnPausa;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblPausa;
+        private System.Windows.Forms.Label lblInicar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblSalir;
+    }
+}
+
